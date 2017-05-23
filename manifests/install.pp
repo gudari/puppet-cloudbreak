ad4536f59a22cc36fbe7bf54057e35ec22c53b6b
gudari <aartola1986@gmail.com>
2017-05-23 22:59:14 +0200

    fix extract command

--- manifests/install.pp
+++ manifests/install.pp
@@ -9,7 +9,7 @@ class cloudbreak::install {
   archive { "${cloudbreak::download_dir}/${cloudbreak::basefilename}":
     ensure          => present,
     extract         => true,
-    extract_command => 'tar xz %s cbd',
+    extract_command => 'tar -xz %s cbd',
     extract_path    => $cloudbreak::extract_dir,
     source          => $cloudbreak::package_url,
     creates         => "${cloudbreak::extract_dir}/cbd",
